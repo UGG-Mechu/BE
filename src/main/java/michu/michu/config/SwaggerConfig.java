183ac1d6718b487bf0c24f05c8620583c96bb013
bikooju <jhjk1234@gmail.com>
2024-12-28 06:52:19 +0900

    commit

--- src/main/java/michu/michu/config/SwaggerConfig.java
+++ src/main/java/michu/michu/config/SwaggerConfig.java
@@ -29,7 +29,7 @@ public class SwaggerConfig {
                         .bearerFormat("JWT"));
 
         return new OpenAPI()
-                .addServersItem(new Server().url("/"))
+                .addServersItem(new Server().url("http://43.201.66.24:8080")) // 실제 서버 URL
                 .info(info)
                 .addSecurityItem(securityRequirement)
                 .components(components);
