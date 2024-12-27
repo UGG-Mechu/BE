7c9d581daee2839fc2db6222907db4d5d3669d3c
bikooju <jhjk1234@gmail.com>
2024-12-27 18:08:21 +0900

    커밋

---
+++ src/main/java/michu/michu/MichuApplication.java
@@ -0,0 +1,13 @@
+package michu.michu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MichuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MichuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/michu/michu/MichuApplicationTests.java
@@ -0,0 +1,13 @@
+package michu.michu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MichuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
